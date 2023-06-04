e6ee84107561659641b566e4b114e2dd0b9b92b8
Lizzz <alejandroraidi@gmail.com>
2023-06-04 10:25:48 -0600

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO department(department_name)
+VALUES ("planning"),
+       ("sales"),
+       ("programmers");
+
+
+INSERT INTO roles(title, salary)
+VALUES ("Senior Oracle", 125000),
+       ("Junior Oracle", 75000),
+       ("Thaumaturge", 300000),
+       ("snake oil sales-person", 60000),
+       ("thieving scoundril", 55000),
+       ("Senior Developer", 302),
+       ("Junior Developer", 150);
+
+
