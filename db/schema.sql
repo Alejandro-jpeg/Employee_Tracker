93a720777611147a61b237eba98fd0b9c528611b
Lizzz <alejandroraidi@gmail.com>
2023-06-05 13:08:07 -0600

    db structure and server

--- db/schema.sql
+++ db/schema.sql
@@ -12,23 +12,25 @@ CREATE TABLE roles(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR (30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT, 
+    department_id INT,
     FOREIGN KEY (department_id)
     REFERENCES department(id)
     ON DELETE SET NULL
 );
 
-CREATE TABLE employee(
-id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE employees(
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 first_name VARCHAR (30) NOT NULL,
 last_name VARCHAR (30) NOT NULL,
 role_id INT,
 manager_id INT,
+manager_name VARCHAR (45),
 FOREIGN KEY (role_id)
-REFERENCES roles(id)
+REFERENCES roles(id) 
 ON DELETE SET NULL
 );
 
 SOURCE seeds.sql
-SELECT *  FROM department;
+SELECT * FROM department;
 SELECT * FROM roles; 
+SELECT * FROM employees;
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,16 +1,25 @@
 INSERT INTO department(department_name)
-VALUES ("planning"),
-       ("sales"),
-       ("programmers");
+VALUES  ("planning"),
+        ("sales"),
+        ("programmers");
 
 
-INSERT INTO roles(title, salary)
-VALUES ("Senior Oracle", 125000),
-       ("Junior Oracle", 75000),
-       ("Thaumaturge", 300000),
-       ("snake oil sales-person", 60000),
-       ("thieving scoundril", 55000),
-       ("Senior Developer", 302),
-       ("Junior Developer", 150);
+INSERT INTO roles(title, salary, department_id)
+VALUES  ("Senior Oracle", 125000, 1),
+        ("Junior Oracle", 75000, 1),
+        ("Thaumaturge", 300000, 1),
+        ("snake oil sales-person", 60000, 2),
+        ("thieving scoundril", 55000, 2),
+        ("Senior Developer", 302, 3),
+        ("Junior Developer", 150, 3);
 
+INSERT INTO employees(first_name, last_name, role_id, manager_id)
+VALUES  ("Lizzzy", "Raidi", 3, null),
+        ("Joan", "Archidi", 1, 1),
+        ("Elsa", "Holmes", 2, 1),
+        ("Daniel", "Ramirez", 4, 6),
+        ("Sebastian", "Nava", 4, 6),
+        ("Maria", "Sanchez", 5, null),
+        ("Laura", "Medicci", 6, null),
+        ("Carlos", "Justamantino", 7, 7);
 
