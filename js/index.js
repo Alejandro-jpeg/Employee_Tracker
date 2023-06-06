4d0ac39680ff31c4b5535f7376fd6b2f9d6b2568
Lizzz <alejandroraidi@gmail.com>
2023-06-06 16:19:48 -0600

    add dptm add role

--- js/index.js
+++ js/index.js
@@ -29,47 +29,137 @@ const question = [
     }
 ]
 
+function addDepartment(){
+inquirer.prompt([
+        {
+            type: 'input',
+            name: 'newDepartment',
+            message: 'Please enter the name of the new Department'
+        }
+    ]).then((answer)=>{
+        const departmentName = answer.newDepartment;
+    db.query(`INSERT INTO department(department_name) VALUES ("${departmentName}");`, (err, results) => {
+        if (err) {
+            console.log(err);
+        }else{
+            console.log(`${departmentName} was added succesfully`);
+            userChoice();
+        }
+    })
+})
+}
+//title, salary, department //TODO: ADD DEPARTMENT AND CHOOSE DEPENDING ON THE EXISTING ONES
+function addRole(){
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'roleTitle',
+            message: 'Please enter the name of the new Role'
+        },
+        {
+            type: 'input',
+            name: 'roleSalary',
+            message: 'Please enter the salary of the Role'
+        }
+        /* {
+            type: 'list',
+            name: 'roleDepartment',
+            message: 'Please enter the department of the Role',
+        } */
+    ]).then((answer)=>{
+        const roleTitle = answer.roleTitle;
+        const roleSalary = answer.roleSalary;
+        const roleDepartment = answer.roleDepartment;
+    db.query(`INSERT INTO roles(title, salary) VALUES ("${roleTitle}, ${roleSalary}");`, (err, results) => {
+        if (err) {
+            console.log(err);
+        }else{
+            console.log(`${roleTitle} was added succesfully`);
+            userChoice();
+        }
+    })
+})
+}
+//first name, last name, role, and manager TODO: SELECT THE ROLE FROM EXISTING ONES AND THE MANAGER FIX ERROR
+function addEmployee(){
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'newFN',
+            message: 'Please enter the first name of the new Employee'
+        },
+        {
+            type: 'input',
+            name: 'newLN',
+            message: 'Please enter the last name of the new Employee'
+        }
+        /* {
+            type: 'input',
+            name: 'newER',
+            message: 'Please select the role of the new Employee'
+        },
+        {
+            type: 'input',
+            name: 'newEM',
+            message: 'Please select the manager of the new Employee'
+        }, */
+    ]).then((answer)=>{
+        const newFN = answer.newFN;
+        const newLN = answer.newLN;
+        const newER = answer.newER;
+        const newEM = answer.newEM;
+    db.query(`INSERT INTO office_db.employee (first_name, last_name) VALUES ("${newFN}, ${newLN}");`, (err, results) => {
+        if (err) {
+            console.log(err);
+        }else{
+            console.log(`new Employee was added succesfully`);
+            userChoice();
+        }
+    })
+})
+}
+
 function userChoice(){
     inquirer.prompt(
         question
     ).then((answer)=>{
-        const choice = answer.choice;
+    const choice = answer.choice;
     switch (choice) {
         case 'View All Departments':
-            db.query('SELECT department_name FROM department', function (err, results) {
+            db.query('SELECT department_name FROM department;', function (err, results) {
                 console.table(results);
                 userChoice();
               });
             break;
 
         case 'View All Roles':
-            db.query('SELECT title, salary, department_name FROM roles JOIN department ON department.id = roles.department_id', function (err, results) {
+            db.query('SELECT title, salary, department_name FROM roles JOIN department ON department.id = roles.department_id;', function (err, results) {
                 console.table(results);
                 userChoice();
               });
             break;
 
         case 'View All Employees'://TODO: MANAGER NAME
-            db.query('SELECT first_name, last_name, title, salary, manager_id FROM office_db.employee JOIN roles ON roles.id = office_db.employee.role_id', function (err, results) {
+            db.query('SELECT first_name, last_name, title, salary, manager_id FROM office_db.employee JOIN roles ON roles.id = office_db.employee.role_id;', function (err, results) {
                 console.table(results);
                 userChoice();
               });
             break;
 
         case 'Add A Department':
-            //View Roles Function
+            addDepartment();
             break;
 
         case 'Add A Role':
-            //View Roles Function
+            addRole();
             break;
 
         case 'Add An Employee':
-            //View Roles Function
+            /* addEmployee(); */
             break;
 
         case 'Update An Employee Role':
-            //View Roles Function
+            //Update Employee Role Function
             break;
 
         case 'Exit':
